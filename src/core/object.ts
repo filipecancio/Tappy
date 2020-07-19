83c62db5bc647d1d8886256ead529389edb7ec3c
Filipe Cancio <filipe.cancio@outlook.com>
2020-07-18 22:19:19 -0300

    Update Lint

--- src/core/object.ts
+++ src/core/object.ts
@@ -1,15 +1,14 @@
-
 /*Retorna true se for objeto e false se não for
-* @param {any} valor
-* @return {boolean} resultado
-* */
-const isObject = (obj) => typeof obj ==='object';
+ * @param {any} valor
+ * @return {boolean} resultado
+ * */
+const isObject = obj => typeof obj === 'object';
 
 /*Converte arrray em object
-* @param chave
-* @param valor
-* @param objeto
-* @return o bojeto*/
-const arrayToObject = (key,value,obj={})=>{
-    return obj;
-}
+ * @param chave
+ * @param valor
+ * @param objeto
+ * @return o bojeto*/
+const arrayToObject = (key, value, obj = {}) => {
+  return obj;
+};
